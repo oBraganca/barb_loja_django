bd07d603f3bc6f61040b143d5fdc8abbcf498293
oBraganca <thayllonryan17@gmail.com>
2022-12-30 09:41:39 -0300

    Rest API

--- barb/urls.py
+++ barb/urls.py
@@ -8,21 +8,22 @@ from django.contrib.auth.views import LogoutView
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'product', views.ProductViewSet)
+router.register(r'users', views.UserViewSet, basename='UserView')
+router.register(r'groups', views.GroupViewSet, basename='GroupView')
+router.register(r'products', views.ListProductsViewSet, basename='ProductsView')
+router.register(r'product/<int:pk>', views.ProductViewSet, basename='ProductView')
 
 urlpatterns = [
     path('', include(router.urls)),
     # path('', views.index, name='index'),
-    path('prods/', views.allProds, name='allprods'),
-    path('view/<int:id>', views.viewproducts, name='view-product'),
-    path('newprod/',views.post_new, name='new-prod'),
-    path('edit/<int:id>', views.editProd, name='edit-product'),
-    path('delete/<int:id>', views.deleteProd, name='delete-Prod'),
-    path('cart/', include("cart.urls", namespace="cart")),
-    path('login/', login_page, name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', register_page, name='register'),
+    # path('prods/', views.allProds, name='allprods'),
+    # path('view/<int:id>', views.viewproducts, name='view-product'),
+    # path('newprod/',views.post_new, name='new-prod'),
+    # path('edit/<int:id>', views.editProd, name='edit-product'),
+    # path('delete/<int:id>', views.deleteProd, name='delete-Prod'),
+    # path('cart/', include("cart.urls", namespace="cart")),
+    # path('login/', login_page, name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    # path('register/', register_page, name='register'),
 ]
     
--- barb/views.py
+++ barb/views.py
@@ -1,9 +1,11 @@
 # SERIALIZER API
 
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import UserSerializer, GroupSerializer, ProductSerializer
+from django.http import Http404
 from .models import *
 
 
@@ -24,13 +26,47 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class ProductViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
+class ListProductsViewSet(viewsets.ModelViewSet):
     queryset = product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        serializer = serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    def get_object(self, pk):
+        try:
+            return product.objects.get(pk=pk)
+        except product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # # # # # # 
 from django.db.models.enums import Choices
